f5141acf34318754c2ba863ddde10d9c9d7f50fb
Jorge Salazar <j-salazarp@hotmail.com>
2022-03-25 19:28:17 +0100

    User model created

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      min: 3,
+      max: 20,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      min: 3,
+      max: 20,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      index:true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      index:true,
+      lowercase: true,
+    },
+    hash_password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin", "super-admin"],
+      default: "user",
+    },
+    contactNumber: { type: String },
+    pofilePicture: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
---
+++ routes/users.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/signin', (req, res, next) => {
+
+
+});
+router.post('/signup', (req, res, next) => {
+
+
+});
+
+module.exports = router;
