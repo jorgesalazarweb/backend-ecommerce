854a280d968743ce6775124cb37f53eed83164e5
Jorge Salazar <j-salazarp@hotmail.com>
2022-03-27 18:01:59 +0200

    signin controller created

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,6 +24,26 @@ exports.signup = async (req, res, next) => {
     const userCreated = await _user.save();
     res.status(201).json({ result: userCreated });
   } catch (err) {
-
+    next(err);
   }
+};
+
+exports.signin = async (req,res,next) =>{
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if(!user || !(await user.comparePassword(password))){
+      const error = new Error('Invalid credentials');
+      error.status = 401;
+      next(error);
+      return;
+  }
+    res.status(201).json({
+      message: 'OK'
+    })
+  } catch (err) {
+    next(err);
+  }
+
+
 };
