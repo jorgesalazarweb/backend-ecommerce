96918121c3c38dd64206783d1c99350c327a6362
Jorge Salazar <j-salazarp@hotmail.com>
2022-03-30 20:24:56 +0200

    Create validations admin

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { signup, signin } = require ('../../controllers/admin/authController');
+const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../../validations/users');
 
-
-router.post('/signin', signin);
-router.post('/signup', signup);
+router.post('/signup',validateSignupRequest,isRequestValidated,signup);
+router.post('/signin',validateSigninRequest,isRequestValidated,signin);
 
 
 module.exports = router;
